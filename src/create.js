07b58e303192e6523e90828164948847cfd81972
Alexander Zinn <azinn@goodleap.com>
2021-11-02 17:44:07 -0400

    refactor create, create utils with handler and dynamodb methods

--- src/create.js
+++ src/create.js
@@ -1,10 +1,8 @@
+import handler from './utils/handler'
+import dynamodb from './utils/dynamodb'
 import * as uuid from 'uuid'
-import AWS from 'aws-sdk'
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient()
-
-export async function main(event) {
-  // Request event is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event) => {
   const data = JSON.parse(event.body)
 
   const params = {
@@ -18,16 +16,7 @@ export async function main(event) {
     }
   }
 
-  try {
-    await dynamoDb.put(params).promise()
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item)
-    }
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: err.message })
-    }
-  }
-}
+  await dynamodb.put(params)
+
+  return params.Item
+})
