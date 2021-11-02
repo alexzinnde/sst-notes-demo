11a87434388018bd3e3867b4b440333a6d5e1ead
Alexander Zinn <azinn@goodleap.com>
2021-11-02 18:19:31 -0400

    remove JSON.parse of event from delete handler

--- src/delete.js
+++ src/delete.js
@@ -2,12 +2,11 @@ import handler from './utils/handler'
 import dynamoDb from './utils/dynamodb'
 
 export const main = handler(async (event) => {
-  const data = JSON.parse(event.body)
   const params = {
     TableName: process.env.TABLE_NAME,
     Key: {
       userId: '123',
-      noteId: data.pathParamters.id
+      noteId: event.pathParameters.id
     }
   }
 
