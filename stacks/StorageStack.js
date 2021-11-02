f838cd8e4948769a3661278e54f2ab617a2b9b7f
Alexander Zinn <azinn@goodleap.com>
2021-11-02 19:28:40 -0400

    Enabling CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,4 +1,4 @@
-import * as sst from "@serverless-stack/resources"
+import * as sst from '@serverless-stack/resources'
 
 export default class StorageStack extends sst.Stack {
   // Public reference to the table
@@ -10,14 +10,26 @@ export default class StorageStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props)
 
-    this.table = new sst.Table(this, "Notes", {
+    this.table = new sst.Table(this, 'Notes', {
       fields: {
         userId: sst.TableFieldType.STRING,
-        noteId: sst.TableFieldType.STRING,
+        noteId: sst.TableFieldType.STRING
       },
-      primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+      primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' }
     })
 
-    this.bucket = new sst.Bucket(this, "Uploads")
+    this.bucket = new sst.Bucket(this, 'Uploads', {
+      s3Bucket: {
+        // Allow client side access to the bucket from a different domain
+        cors: [
+          {
+            maxAge: 3000,
+            allowedOrigins: ['*'],
+            allowedHeaders: ['*'],
+            allowedMethods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD']
+          }
+        ]
+      }
+    })
   }
 }
